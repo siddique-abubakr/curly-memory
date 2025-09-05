afa792e947116606a498f4d11b154d3a1470029a
Abubakar Siddique <abubakar.siddique@arbisoft.com>
2025-09-06 02:08:52 +0500

    refactor: more specific directory structure for services

--- src/services/__init__.py
+++ src/services/__init__.py
@@ -1,6 +1,6 @@
-from .board_service import BoardService
-from .sprint_service import SprintService
-from .issue_service import IssueService
-from .jira_analyzer import JiraAnalyzer
+from .jira.board_service import BoardService as JiraBoardService
+from .jira.sprint_service import SprintService as JiraSprintService
+from .jira.issue_service import IssueService as JiraIssueService
+from .jira.jira_analyzer import JiraAnalyzer
 
-__all__ = ["BoardService", "SprintService", "IssueService", "JiraAnalyzer"]
+__all__ = ["JiraBoardService", "JiraSprintService", "JiraIssueService", "JiraAnalyzer"]
